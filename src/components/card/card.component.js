fc219d7febceafb4bc2ee0d5643c6c2983b75708
87werner <twerner15@googlemail.com>
2021-02-06 20:07:44 +0000

    email and city added to card

--- src/components/card/card.component.js
+++ src/components/card/card.component.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import './card.styles.css'
+import React from "react";
+import "./card.styles.css";
 
 const Card = (props) => {
-    return (
-       
-        <div className="card-container" >
-           <img alt="monster" src={ `https://robohash.org/${props.monster.id}?set=set2`} />
-             <h1>{ props.monster.name }</h1>
-        </div>
-    )
-}
+  return (
+    <div className="card-container">
+    <h2>{props.monster.name}</h2>
+      <img
+        alt="monster"
+        src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+      />
+     
+      <p>email: {props.monster.email}</p>
+      <p>city: {props.monster.address.city}</p>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
